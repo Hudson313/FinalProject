2195ec2cac88dfff7f3571dfaed0eeaa549177c7
SirPhilly <philly2783@gmail.com>
2016-10-11 15:55:02 -0400

    Add project files.

---
+++ TeamApolloFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamApolloFinal.Startup))]
+namespace TeamApolloFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
