6f462ad1d2ce3c242b757e84313c79d8abf22369
SirPhilly <philly2783@gmail.com>
2016-10-17 16:08:59 -0400

    Sheila did some things.

--- TeamApolloFinal/Controllers/HomeController.cs
+++ TeamApolloFinal/Controllers/HomeController.cs
@@ -32,10 +32,110 @@ namespace TeamApolloFinal.Controllers
                     ViewBag.Quote = quote.Motivation.ToString();
                 }
             }
+            List<CarouselData> dotnet = new List<CarouselData>()
+            { new CarouselData() {  linkUrl= "https://www.github.com", src=@"/Content/Images/github.jpg", alt="Owl Image"} ,
+            new CarouselData() {  linkUrl= "https://social.msdn.microsoft.com/search/en-US", src=@"/Content/Images/MDSN.jpg", alt="MSDN"},
+            new CarouselData() { linkUrl= "http://www.w3schools.com/", src=@"/Content/Images/W3Schools.jpg", alt="Owl Image"},
+            new CarouselData() { linkUrl= "https://www.reddit.com/r/learnprogramming/", src=@"/Content/Images/Reddit.jpg", alt="Owl Image"}};
+
+
+            List<CarouselData> java = new List<CarouselData>()
+            { new CarouselData() { linkUrl= "https://www.reddit.com/r/learnprogramming/", src=@"/Content/Images/MDSN.jpg", alt="Owl Image"} ,
+            new CarouselData() {  linkUrl= "https://social.msdn.microsoft.com/search/en-US", src=@"/Content/Images/MDSN.jpg", alt="MSDN"},
+            new CarouselData() { linkUrl= "http://www.w3schools.com/", src=@"/Content/Images/W3Schools.jpg", alt="Owl Image"},
+            new CarouselData() { linkUrl= "https://www.reddit.com/r/learnprogramming/", src=@"/Content/Images/Reddit.jpg", alt="Owl Image"}};
+
+
+            List<CarouselData> everything = java;
+            /*
+             *   <div class="item"><a href="https://www.github.com" target="_blank"><img src="~/Content/Images/github.jpg" alt="Owl Image"></a></div>
+        
+        <div class="item"><a href="https://social.msdn.microsoft.com/search/en-US" target="_blank"><img src="~/Content/Images/MDSN.jpg" alt="Owl Image"></a></div>
+        <div class="item"><a href="http://www.w3schools.com/" target="_blank"><img src="~/Content/Images/W3Schools.jpg" alt="Owl Image"></a></div>
+     
+       
+        <div class="item"><a href="https://www.reddit.com/r/learnprogramming/" target="_blank"><img src="~/Content/Images/Reddit.jpg" alt="Owl Image"></a></div>
+        <div class="item"><a href="http://stackoverflow.com/" target="_blank"><img src="~/Content/Images/StackOverflow.jpg" alt="Owl Image"></a></div>
+        <div class="item"><a href="https://www.edx.org/course/subject/computer-science" target="_blank"><img src="~/Content/Images/EdX.jpg" alt="Owl Image"></a></div>
+         <div class="item"><a href="https://www.codecademy.com/" target="_blank"><img src="~/Content/Images/Codeacademy.jpg" alt="Owl Image"></a></div>
+        <div class="item"><a href="https://www.khanacademy.org/computing/computer-programming" target="_blank"><img src="~/Content/Images/KhanAcademy.jpg" alt="Owl Image"></a></div>
+        <div class="item"><a href="http://lifehacker.com/" target="_blank"><img src="~/Content/Images/LifeHacker.jpg" alt="Owl Image"></a></div>
+
+             * */
+            ViewBag.everything = everything;
 
             return View(dbQuotes);
         }
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            List<Apollo> dbQuotes = dbContext.Apolloes.ToList();
+            //List<Apollo> quoteDisplayed = new List<Apollo>();
 
+            RandomNumberGenerator randNum = new RandomNumberGenerator();
+            int randQuoteID = randNum.NumberBetween(1, dbQuotes.Count());
+
+            foreach (Apollo quote in dbQuotes)
+            {
+                if (quote.QuoteID == randQuoteID)
+                {
+                    //quoteDisplayed.Add(quote);
+
+                    //return View(quoteDisplayed);
+                    ViewBag.Quote = quote.Motivation.ToString();
+                }
+            }
+            List<CarouselData> dotnet = new List<CarouselData>()
+            { new CarouselData() { linkUrl= "https://www.github.com", src=@"/Content/Images/github.jpg", alt="Owl Image"} ,
+            new CarouselData() {  linkUrl= "https://social.msdn.microsoft.com/search/en-US", src=@"/Content/Images/MDSN.jpg", alt="MSDN"},
+            new CarouselData() { linkUrl= "http://www.w3schools.com/", src=@"/Content/Images/W3Schools.jpg", alt="Owl Image"},
+            new CarouselData() { linkUrl= "https://www.reddit.com/r/learnprogramming/", src=@"/Content/Images/Reddit.jpg", alt="Owl Image"}};
+
+
+            List<CarouselData> java = new List<CarouselData>()
+            {new CarouselData() { linkUrl= "https://www.reddit.com/r/learnprogramming/", src=@"/Content/Images/MDSN.jpg", alt="Owl Image"} ,
+            new CarouselData() {  linkUrl= "https://social.msdn.microsoft.com/search/en-US", src=@"/Content/Images/MDSN.jpg", alt="MSDN"},
+            new CarouselData() { linkUrl= "http://www.w3schools.com/", src=@"/Content/Images/W3Schools.jpg", alt="Owl Image"},
+            new CarouselData() { linkUrl= "https://www.reddit.com/r/learnprogramming/", src=@"/Content/Images/Reddit.jpg", alt="Owl Image"}};
+
+            List<CarouselData> all = new List<CarouselData>()
+            {
+
+            };
+            List<CarouselData> everything ; ;
+            switch (form["track"])
+            {
+                case "java":
+                    everything = java;
+                    break;
+                case "net":
+                    everything =dotnet;
+                    break;
+                default:
+                    everything = all;
+                    break;
+            }
+
+           
+            /*
+             *   <div class="item"><a href="https://www.github.com" target="_blank"><img src="~/Content/Images/github.jpg" alt="Owl Image"></a></div>
+        
+        <div class="item"><a href="https://social.msdn.microsoft.com/search/en-US" target="_blank"><img src="~/Content/Images/MDSN.jpg" alt="Owl Image"></a></div>
+        <div class="item"><a href="http://www.w3schools.com/" target="_blank"><img src="~/Content/Images/W3Schools.jpg" alt="Owl Image"></a></div>
+     
+       
+        <div class="item"><a href="https://www.reddit.com/r/learnprogramming/" target="_blank"><img src="~/Content/Images/Reddit.jpg" alt="Owl Image"></a></div>
+        <div class="item"><a href="http://stackoverflow.com/" target="_blank"><img src="~/Content/Images/StackOverflow.jpg" alt="Owl Image"></a></div>
+        <div class="item"><a href="https://www.edx.org/course/subject/computer-science" target="_blank"><img src="~/Content/Images/EdX.jpg" alt="Owl Image"></a></div>
+         <div class="item"><a href="https://www.codecademy.com/" target="_blank"><img src="~/Content/Images/Codeacademy.jpg" alt="Owl Image"></a></div>
+        <div class="item"><a href="https://www.khanacademy.org/computing/computer-programming" target="_blank"><img src="~/Content/Images/KhanAcademy.jpg" alt="Owl Image"></a></div>
+        <div class="item"><a href="http://lifehacker.com/" target="_blank"><img src="~/Content/Images/LifeHacker.jpg" alt="Owl Image"></a></div>
+
+             * */
+            ViewBag.everything = everything;
+
+            return View(dbQuotes);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -59,9 +159,9 @@ namespace TeamApolloFinal.Controllers
         [HttpPost]
         public ActionResult Login(User admin)
         {
-            foreach(User name in dbContext.Users.ToList())
+            foreach (User name in dbContext.Users.ToList())
             {
-                if((name.UserName == admin.UserName) && (name.Password == admin.Password))
+                if ((name.UserName == admin.UserName) && (name.Password == admin.Password))
                 {
                     return RedirectToAction("AddQuote");
                 }
---
+++ TeamApolloFinal/Models/CarouselData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamApolloFinal.Models
+{
+    public class CarouselData
+    {
+        //<div class="item"><a href = "https://www.github.com" target="_blank"><img src = "~/Content/Images/github.jpg" alt="Owl Image"></a></div>
+        public string linkUrl { get; set; }
+        public string src { get; set; }
+        public string alt { get; set; }
+    }
+}
